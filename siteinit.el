d90f264480779a760c278d15e361e0efdc205bbe
Naofumi Hattori <hattorix0@gmail.com>
2011-06-09 15:58:08 +0900

    テスト駆動開発支援を追加

--- siteinit.el
+++ siteinit.el
@@ -307,6 +307,20 @@
 
 ;;
 ;=======================================================================
+; test-case-mode.el
+; - テスト駆動開発支援
+;
+; - Project Homepage
+; http://nschum.de/src/emacs/test-case-mode/
+;=======================================================================
+(require 'test-case-mode)
+(add-hook 'find-file-hook 'enable-test-case-mode-if-test)
+
+;; コンパイル後に、テストの自動実行
+;(add-hook 'compilation-finish-functions 'test-case-compilation-finish-run-all)
+
+;;
+;=======================================================================
 ; dabbrev-ja.el
 ; - 日本語で dabbrev(動的略語補完) を使う
 ;=======================================================================
