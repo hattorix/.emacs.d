3e86c45c1d38d79e9c9e7cb80898dd32bf9088d1
Naofumi Hattori <hattorix0@gmail.com>
2011-06-09 15:58:29 +0900

    ruby 開発用の設定を追加

--- siteinit.el
+++ siteinit.el
@@ -124,6 +124,40 @@
 
 ;;
 ;=======================================================================
+; ruby-mode.el
+; - ruby プログラミング用のメジャーモード
+;
+; - インストール
+; # aptitude install ruby-elisp
+;
+; - Project homepage
+; http://www.ruby-lang.org/ja/
+;=======================================================================
+(when (locate-library "ruby-mode")
+  ;; ruby-mode
+  (autoload 'ruby-mode "ruby-mode" nil t)
+
+  (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+
+  ;; ruby-electric - 閉じ括弧とか end を保管する
+  (when (locate-library "ruby-electric")
+    (require 'ruby-electric)
+    (add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t))))
+
+  ;; inf-ruby - irb 実行環境
+  (when (locate-library "inf-ruby")
+    (autoload 'run-ruby "inf-ruby" nil)
+    (autoload 'inf-ruby-keys "inf-ruby" nil)
+    (add-hook 'ruby-mode-hook '(lambda () (inf-ruby-keys))))
+
+  ;; rubydb - ruby デバッガ
+  (when (locate-library "rubydb3x")
+    (autoload 'rubydb "rubydb3x" nil t))
+  )
+
+;;
+;=======================================================================
 ; minibuf-isearch.el
 ; - ミニバッファで isearch 的な検索をする
 ;
