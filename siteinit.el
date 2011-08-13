956bc60c3b2b056f54e90a47606c446068fd6aa6
Naofumi Hattori <hattorix0@gmail.com>
2011-08-14 03:08:31 +0900

    ウィンドウの分割に関わる処理を変更
    - Shift+カーソルキーでウィンドウを移動
    - 分割したウィンドウのリサイズ機能を追加

--- siteinit.el
+++ siteinit.el
@@ -648,6 +648,10 @@
 ;; ファイル名や URL の上で C-x C-f すると、そのファイル名や URL が開ける
 (ffap-bindings)
 
+;; Shift+カーソルキー で、分割したウィンドウ間を移動
+(windmove-default-keybindings)
+(setq windmove-wrap-around t)
+
 ;;
 ;=======================================================================
 ; 表示設定
@@ -929,6 +933,42 @@
     (add-to-list 'tab-stop-list num t)
     (setq num (+ num tab-width))))
 
+;; 分割したウィンドウのリサイズ
+(defun my-window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        action c)
+    (catch 'end-flag
+      (while t
+        (setq action
+              (read-key-sequence-vector (format "size[%dx%d]"
+                                                (window-width)
+                                                (window-height))))
+        (setq c (aref action 0))
+        (cond ((= c ?l)
+               (enlarge-window-horizontally dx))
+              ((= c ?h)
+               (shrink-window-horizontally dx))
+              ((= c ?j)
+               (enlarge-window dy))
+              ((= c ?k)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               (let ((last-command-char (aref action 0))
+                     (command (key-binding action)))
+                 (when command
+                   (call-interactively command)))
+               (message "Quit")
+               (throw 'end-flag t)))))))
+
 ;;----------------------------------------------
 ;; カーソル行を上下に移動
 ;;----------------------------------------------
@@ -1001,5 +1041,6 @@
 
 (global-set-key "\C-cg" 'magit-status)         ;magit実行
 (global-set-key "\C-cs" 'scheme-other-window)  ;scheme実行
+(global-set-key "\C-cr" 'my-window-resizer)    ;ウィンドウのリサイズ
 
 (put 'set-goal-column 'disabled nil)
