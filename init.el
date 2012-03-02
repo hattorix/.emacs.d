22872aafc50dd16c6e8950272c906c9b5e96a435
Naofumi Hattori <hattorix0@gmail.com>
2012-03-03 01:31:36 +0900

    auto-complete の情報源に gtags を追加

--- init.el
+++ init.el
@@ -760,6 +760,11 @@
 
 ;; 4 文字以上の単語の時に補完を開始する
 (setq ac-auto-start 4)
+;; 補完の情報源
+(setq ac-sources '(ac-source-yasnippet
+                   ac-source-dictionary
+                   ac-source-gtags
+                   ac-source-words-in-buffer))
 
 ;; キーマップ
 (define-key ac-completing-map "\M-/" 'ac-stop) ; 保管の停止
