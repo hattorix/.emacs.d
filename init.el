829ed56da6c3964dee3fc7ab0f9bc38735e4d8cb
Naofumi Hattori <hattorix0@gmail.com>
2012-03-08 16:46:35 +0900

    multi-term を追加

--- init.el
+++ init.el
@@ -546,6 +546,48 @@
 
 ;;
 ;=======================================================================
+; multi-term.el
+; - term の拡張
+;
+; - Project wiki
+; http://www.emacswiki.org/emacs/MultiTerm/
+;=======================================================================
+(require 'multi-term)
+
+(add-hook 'term-mode-hook
+          '(lambda ()
+             ;; 配色設定
+             (setq term-default-fg-color "white")
+             (setq term-default-bg-color "#222222")
+             (setq ansi-term-color-vector [unspecified "#000000" "#FF0000" "#00FF00" "#AA5500" "#8080FF" "#AA00AA" "#00AAAA" "#AAAAAA"])
+
+             ;; 実行する shell の設定
+             (setq multi-term-program shell-file-name)
+
+             ;; global-hl-line-mode を term-mode で無効にする
+             (set (make-local-variable 'hl-line-range-function)
+                  (lambda () '(0 . 0)))
+
+             ;; キーバインド
+             (setq term-bind-key-alist
+                   '(
+                     ("C-c C-c" . term-interrupt-subjob)
+                     ("C-h" . term-send-backspace)
+                     ("C-m" . term-send-raw)
+                     ("C-r" . term-send-reverse-search-history)
+                     ("C-y" . term-paste)
+                     ("M-b" . term-send-backward-word)
+                     ("M-d" . term-send-forward-kill-word)
+                     ("M-f" . term-send-forward-word)
+                     ("M-n" . next-line)
+                     ("M-p" . previous-line)
+                     ("M-r" . isearch-backward)
+                     ("M-s" . isearch-forward)
+                     ))
+             ))
+
+;;
+;=======================================================================
 ; gtags.el
 ; - GNU Global を使ったタグジャンプ
 ;
@@ -576,16 +618,20 @@
 ;=======================================================================
 (when (require 'tabbar nil t)
 
-  ;; タブをグループ化しない
+  ;; タブのグループ化設定
   (setq tabbar-buffer-groups-function
-        (lambda () (list "All Buffers")))
+        (lambda ()
+          (cond
+           ((eq major-mode 'dired-mode) '("Dired"))
+           ((eq major-mode 'term-mode) '("Terminal"))
+           (t (list "User Buffer")))))
 
   ;; `*scratch*' 以外の ` *'から始まるバッファーをリストしない
   (setq tabbar-buffer-list-function
         (lambda ()
           (remove-if
            (lambda (buffer)
-             (unless (string-match-p "\\*\\(scratch\\|tmp\\)" (buffer-name buffer))
+             (unless (string-match-p "\\*\\(scratch\\|tmp\\|terminal\\)" (buffer-name buffer))
                (find (aref (buffer-name buffer) 0) " *")))
            (buffer-list))))
 
@@ -1347,6 +1393,7 @@
 
 (global-set-key "\C-cg" 'magit-status)         ;magit実行
 (global-set-key "\C-cs" 'scheme-other-window)  ;scheme実行
+(global-set-key "\C-ct" 'multi-term)           ;ターミナルを開く
 (global-set-key "\C-cr" 'my-window-resizer)    ;ウィンドウのリサイズ
 
 (put 'set-goal-column 'disabled nil)
