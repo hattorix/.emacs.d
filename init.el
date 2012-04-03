84e7dc844a75c8f80474aba82f3bf65c73938d51
Naofumi Hattori <hattorix0@gmail.com>
2012-04-03 12:04:55 +0900

    dired の各種設定を追加

--- init.el
+++ init.el
@@ -108,11 +108,18 @@
 ; dired
 ;=======================================================================
 
-;; r -> C-x C-s でファイル名の編集
-(require 'wdired)
-
 ;; 二画面モード
 (setq dired-dwim-target t)
+;; コピー、削除は再帰的に
+(setq dired-recursive-copies 'always)
+(setq dired-recursive-deletes 'always)
+;; ディレクトリを先頭に表示させる
+(setq dired-listing-switches "-lh --group-directories-first")
+;; dired-find-alternate-fileを有効に
+(put 'dired-find-alternate-file 'disabled nil)
+
+;; r -> C-x C-s でファイル名の編集
+(require 'wdired)
 
 ;; スペースでマークする (FD like)
 (defun dired-toggle-mark (arg)
@@ -127,8 +134,16 @@
     ;(dired-previous-line 1)
     ))
 
+;; dired-find-alternate-file の親ディレクトリ移動版
+(defun dired-discard-up-directory ()
+  (interactive)
+  (find-alternate-file ".."))
+
 (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode) ;rで編集モードに
 (define-key dired-mode-map " " 'dired-toggle-mark)            ;スペースでマークをトグル
+;(define-key dired-mode-map [return] 'dired-find-alternate-file)
+(define-key dired-mode-map "\C-h" 'dired-up-directory)
+(define-key dired-mode-map [tab] 'other-window-or-split)
 
 ;;
 ;=======================================================================
