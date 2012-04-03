d7417e790fd9cb84ef48fc285c64435f4cade5c2
Naofumi Hattori <hattorix0@gmail.com>
2012-04-03 12:04:19 +0900

    dsvn を導入

--- init.el
+++ init.el
@@ -800,6 +800,17 @@
 
 ;;
 ;=======================================================================
+; dsvn.el
+; - Subversion
+;
+; - Source code
+; http://svn.apache.org/repos/asf/subversion/trunk/contrib/client-side/emacs/
+;=======================================================================
+(autoload 'svn-status "dsvn" "Run `svn status'." t)
+(autoload 'svn-update "dsvn" "Run `svn update'." t)
+
+;;
+;=======================================================================
 ; mercurial.el
 ; - Mercurial 付属の elisp
 ;
