54bd1b4f615b4c108f1bfa8d168e5de6f5c76e88
Naofumi Hattori <hattorix0@gmail.com>
2013-04-18 18:33:46 +0900

    Java Script モードの設定を init-loader に移動

--- init.el
+++ init.el
@@ -156,31 +156,6 @@
 
 ;;
 ;=======================================================================
-; js2-mode.el
-; - java-script プログラミング用のメジャーモード
-;
-; - Project homepage
-; http://code.google.com/p/js2-mode/
-;=======================================================================
-(autoload 'js2-mode "js2" nil t)
-
-;; インデントの関数の再設定
-(add-hook 'js2-mode-hook
-          #'(lambda ()
-              (require 'js)
-
-              ;; インデントスタイルを Google JavaScript Style に合わせる
-              (set (make-local-variable 'indent-line-function) 'js-indent-line)
-
-              ;; インデント サイズの設定
-              (setq js-indent-level 2
-                    js-expr-indent-offset 2
-                    indent-tabs-mode nil)))
-
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
-;;
-;=======================================================================
 ; coffee-mode.el
 ; - coffee-script プログラミング用のメジャーモード
 ;
---
+++ inits/42-javascript.el
@@ -0,0 +1,30 @@
+;;--------------------------------------------------
+;; File name    :   42-javascript.el
+;;              :   Java Script のソースコード編集
+;;              :
+;;--------------------------------------------------
+;;
+;=======================================================================
+; js2-mode.el
+; - java-script プログラミング用のメジャーモード
+;
+; - Project homepage
+; http://code.google.com/p/js2-mode/
+;=======================================================================
+(autoload 'js2-mode "js2" nil t)
+
+;; インデントの関数の再設定
+(add-hook 'js2-mode-hook
+          #'(lambda ()
+              (require 'js)
+
+              ;; インデントスタイルを Google JavaScript Style に合わせる
+              (set (make-local-variable 'indent-line-function) 'js-indent-line)
+
+              ;; インデント サイズの設定
+              (setq js-indent-level 2
+                    js-expr-indent-offset 2
+                    indent-tabs-mode nil)))
+
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
