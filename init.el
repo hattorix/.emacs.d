37388ace2001ea00f1a76a676396ab1ac5ddea20
Naofumi Hattori <hattorix0@gmail.com>
2013-04-18 18:36:24 +0900

    CoffeeScript モードの設定を init-loader に移動

--- init.el
+++ init.el
@@ -17,7 +17,6 @@
 (add-to-list 'load-path (concat siteinit-path "elisp/init-loader") t)
 (add-to-list 'load-path (concat siteinit-path "anything") t)
 (add-to-list 'load-path (concat siteinit-path "auto-complete") t)
-(add-to-list 'load-path (concat siteinit-path "coffee-mode") t)
 (add-to-list 'load-path (concat siteinit-path "magit") t)
 (add-to-list 'load-path (concat siteinit-path "mmm-mode") t)
 (add-to-list 'load-path (concat siteinit-path "popwin") t)
@@ -109,22 +108,6 @@
   ;; flymake-err-line-patterns は python の設定で吸収
   )
 
-;-----------------------------------------------
-; CoffeeScript で flymake を使う
-;-----------------------------------------------
-(when (executable-find "coffeelint")
-  (defun flymake-coffeescript-init ()
-    (flymake-simple-generic-init
-     ;; なぜか動かない
-     ;;"coffeelint" (append (if (file-exists-p "~/.coffeelint.conf")
-     ;;                       '("-f" "~/.coffeelint.conf"))
-     ;;                     (list "--csv"))))
-     "coffeelint" '("--csv")))
-
-  (add-hook 'coffee-mode-hook 'flymake-mode-if-enable-buffer)
-  (push '("\\.coffee$" flymake-coffeescript-init) flymake-allowed-file-name-masks)
-  (push '("^\\([^,]+\\),\\([0-9]+\\),\\(\\(warn\\|error\\),\\(.*\\)\\)$" 1 2 nil 3) flymake-err-line-patterns)
-  )
 
 ;;
 ;=======================================================================
@@ -155,48 +138,6 @@
 (require 'csharp-mode)
 
 ;;
-;=======================================================================
-; coffee-mode.el
-; - coffee-script プログラミング用のメジャーモード
-;
-; - Project homepage
-; https://github.com/defunkt/coffee-mode
-;=======================================================================
-(when (require 'coffee-mode nil t)
-  ;; coffee-mode
-  (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-  (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-
-  (defun coffee-custom ()
-    "coffee-mode-hook"
-
-    ;; CoffeeScript uses two spaces.
-    (make-local-variable 'tab-width)
-    (set 'tab-width 2)
-
-    ;; If you don't have js2-mode
-    ;(setq coffee-js-mode 'javascript-mode)
-
-    ;; If you don't want your compiled files to be wrapped
-    (setq coffee-args-compile '("-c" "--bare"))
-
-    ;; *Messages* spam
-    (setq coffee-debug-mode t)
-
-    ;; Emacs key binding
-    (define-key coffee-mode-map "\M-r" 'coffee-compile-buffer)
-
-    ;; Riding edge.
-    (setq coffee-command "coffee")
-
-    ;; Compile '.coffee' files on every save
-    (and (file-exists-p (buffer-file-name))
-         (file-exists-p (coffee-compiled-file-name))
-         (coffee-cos-mode t)))
-
-  (add-hook 'coffee-mode-hook 'coffee-custom)
-  )
-
 ;;
 ;=======================================================================
 ; powershell-mode.el
