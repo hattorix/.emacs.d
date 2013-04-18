53da6498093f543855e42322acd9ba40f6b034d1
Naofumi Hattori <hattorix0@gmail.com>
2013-04-18 18:31:54 +0900

    PHP モードの設定を init-loader に移動

--- init.el
+++ init.el
@@ -110,14 +110,6 @@
   )
 
 ;-----------------------------------------------
-; PHP で flymake を使う
-;-----------------------------------------------
-(when (executable-find "php")
-  (add-hook 'php-mode-hook 'flymake-mode-if-enable-buffer)
-  (push '("\\.php[345]?$" flymake-php-init) flymake-allowed-file-name-masks)
-  )
-
-;-----------------------------------------------
 ; CoffeeScript で flymake を使う
 ;-----------------------------------------------
 (when (executable-find "coffeelint")
@@ -161,38 +153,6 @@
 ; http://www.emacswiki.org/emacs/CSharpMode
 ;=======================================================================
 (require 'csharp-mode)
-
-;;
-;=======================================================================
-; php-mode.el
-; - php プログラミング用のメジャーモード
-;
-; - Project homepage
-; http://sourceforge.net/projects/php-mode/
-;=======================================================================
-(require 'php-mode)
-
-(add-hook 'php-mode-hook
-          '(lambda ()
-             ;; インデントスタイルの設定(gnu,bsd,k&r,stroustrup,linux,java)
-             (c-set-style "stroustrup")
-             ;; 連続する空白の一括削除
-             (c-toggle-hungry-state t)
-             ;; コメント行のインデント
-             (setq c-comment-only-line-offset 0)
-             ;; コメントのスタイル
-             (setq comment-start "// "
-                   comment-end   ""
-                   comment-start-skip "// *")
-             ;; `;' を入力したら、自動改行+インデント
-             (c-toggle-auto-hungry-state 1)
-             ;; インデント サイズの設定
-             (setq tab-width 4
-                   c-basic-offset 4
-                   c-hanging-comment-ender-p nil
-                   indent-tabs-mode nil)))
-
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 
 ;;
 ;=======================================================================
