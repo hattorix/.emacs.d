34674e046ee026d14ca15f4f3f076f28985c5b03
Naofumi Hattori <hattorix0@gmail.com>
2013-04-18 16:18:33 +0900

    バッファ関連の設定を init-loader に移動

---
+++ inits/03-buffer.el
@@ -0,0 +1,59 @@
+;;--------------------------------------------------
+;; File name    :   03-buffer.el
+;;              :   バッファ設定
+;;              :
+;;--------------------------------------------------
+;;
+;=======================================================================
+; バッファ関連の設定
+;=======================================================================
+
+;; バッファの切り替えに iswitchb を使う
+(iswitchb-mode t)
+
+;; C-xb 後の C-s, C-r で、プレビュー表示する
+(defadvice iswitchb-exhibit
+  (after
+   iswitchb-exhibit-with-display-buffer
+   activate)
+  "選択している buffer を window に表示してみる。"
+  (when (and
+         (eq iswitchb-method iswitchb-default-method)
+         iswitchb-matches)
+    (select-window
+     (get-buffer-window (cadr (buffer-list))))
+    (let ((iswitchb-method 'samewindow))
+      (iswitchb-visit-buffer
+       (get-buffer (car iswitchb-matches))))
+    (select-window (minibuffer-window))))
+
+(defun my-make-scratch (&optional arg)
+  (interactive)
+  (progn
+    ;; "*scratch*" を作成して buffer-list に放り込む
+    (set-buffer (get-buffer-create "*scratch*"))
+    (funcall initial-major-mode)
+    (erase-buffer)
+    (when (and initial-scratch-message (not inhibit-startup-message))
+      (insert initial-scratch-message))
+    (or arg (progn (setq arg 0)
+                   (switch-to-buffer "*scratch*")))
+    (cond ((= arg 0) (message "*scratch* is cleared up."))
+          ((= arg 1) (message "another *scratch* is created")))))
+
+(defun my-buffer-name-list ()
+  (mapcar (function buffer-name) (buffer-list)))
+
+(add-hook 'kill-buffer-query-functions
+          ;; *scratch* バッファで kill-buffer したら内容を消去するだけにする
+          (function (lambda ()
+                      (if (string= "*scratch*" (buffer-name))
+                          (progn (my-make-scratch 0) nil)
+                        t))))
+
+(add-hook 'after-save-hook
+          ;; *scratch* バッファの内容を保存したら *scratch* バッファを新しく作る
+          (function (lambda ()
+                      (unless (member "*scratch*" (my-buffer-name-list))
+                        (my-make-scratch 1)))))
+
