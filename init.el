6e7ac64e89233013ef53a72e9a92afe320dcfe48
Naofumi Hattori <hattorix0@gmail.com>
2013-04-18 19:37:04 +0900

    markdown-mode の設定を init-loader に移動

--- init.el
+++ init.el
@@ -112,18 +112,6 @@
 
 ;;
 ;=======================================================================
-; markdown-mode.el
-; - Markdown ファイル編集用のメジャーモード
-;
-; - Project page
-; http://jblevins.org/projects/markdown-mode/
-;=======================================================================
-(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
-
-;;
-;=======================================================================
 ; pkgbuild-mode.el
 ; - PKGBUILD ファイル編集用のメジャーモード
 ;
---
+++ inits/50-markdown.el
@@ -0,0 +1,10 @@
+;;
+;=======================================================================
+; markdown-mode.el
+; - Markdown ファイル編集用のメジャーモード
+;
+; - Project page
+; http://jblevins.org/projects/markdown-mode/
+;=======================================================================
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
