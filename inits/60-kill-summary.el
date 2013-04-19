46a83618739f6ae5951582706022bd82d396e5eb
Naofumi Hattori <hattorix0@gmail.com>
2013-04-19 16:48:49 +0900

    kill-summary.el の設定を init-loader に移動

---
+++ inits/60-kill-summary.el
@@ -0,0 +1,9 @@
+;;
+;=======================================================================
+; kill-summary.el
+; - kill-ring の内容を一覧表示してそこから yank する
+;
+; - Project page
+; http://emacs-session.sourceforge.net/
+;=======================================================================
+(autoload 'kill-summary "kill-summary" nil t)
