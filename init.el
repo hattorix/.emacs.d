6fb8876db2ece419660c8f631d91aca816ddafe6
Naofumi Hattori <hattorix0@gmail.com>
2013-04-19 15:13:03 +0900

    dsvn を ELPA に移行し、設定を init-loader に移動

--- init.el
+++ init.el
@@ -132,17 +132,6 @@
 
 ;;
 ;=======================================================================
-; dsvn.el
-; - Subversion
-;
-; - Source code
-; http://svn.apache.org/repos/asf/subversion/trunk/contrib/client-side/emacs/
-;=======================================================================
-(autoload 'svn-status "dsvn" "Run `svn status'." t)
-(autoload 'svn-update "dsvn" "Run `svn update'." t)
-
-;;
-;=======================================================================
 ; mercurial.el
 ; - Mercurial 付属の elisp
 ;
---
+++ inits/45-dsvn.el
@@ -0,0 +1,10 @@
+;;
+;=======================================================================
+; dsvn.el
+; - Subversion
+;
+; - Source code
+; http://svn.apache.org/repos/asf/subversion/trunk/contrib/client-side/emacs/
+;=======================================================================
+(autoload 'svn-status "dsvn" "Run `svn status'." t)
+(autoload 'svn-update "dsvn" "Run `svn update'." t)
