59d6b9eb7f83ea6509be57fdec700f73cece78a6
Naofumi Hattori <hattorix0@gmail.com>
2013-04-22 21:07:03 +0900

    undo-tree.el を導入

---
+++ inits/60-undo-tree.el
@@ -0,0 +1,9 @@
+;;
+;=======================================================================
+; undo-tree.el
+;
+; - Emacs wiki
+; http://www.emacswiki.org/emacs/UndoTree
+;=======================================================================
+;(require 'undo-tree)
+(global-undo-tree-mode)
