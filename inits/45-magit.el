0a2e402cb583e9841d981d76b771a8c8889c7b9c
Naofumi Hattori <hattorix0@gmail.com>
2014-11-11 17:24:34 +0900

    update magit

--- inits/45-magit.el
+++ inits/45-magit.el
@@ -7,8 +7,12 @@
 ; - Project page
 ; http://philjackson.github.com/magit/
 ;=======================================================================
+(add-to-list 'load-path (concat siteinit-path "elisp/git-modes") t)
 (add-to-list 'load-path (concat siteinit-path "elisp/magit") t)
 
+(eval-after-load 'info
+  '(progn (info-initialize)
+          (add-to-list 'Info-directory-list (concat siteinit-path "elisp/magit"))))
 (autoload 'magit-status "magit" nil t)
 
 ;; vc-mode 無効化
